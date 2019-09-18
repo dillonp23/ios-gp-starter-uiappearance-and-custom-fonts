06cea5a524a7b4ae048c15026836a7cf4511bf12
dillonp23 <dillonp.lambda@gmail.com>
2019-09-17 20:22:14 -0700

    Add appearanceHelper code with appearance proxy

--- Lambda Messages Theming Starter/Lambda Messages Theming/Helpers/AppearanceHelper.swift
+++ Lambda Messages Theming Starter/Lambda Messages Theming/Helpers/AppearanceHelper.swift
@@ -2,8 +2,45 @@
 //  AppearanceHelper.swift
 //  Lambda Messages Theming
 //
-//  Created by Andrew R Madsen on 1/28/19.
+//  Created by Dillon P on 9/17/19.
 //  Copyright © 2019 Lambda School. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+enum AppearanceHelper {
+    
+    //static means belongs to type not the specific instance of our enum
+    
+    static var lambdaRed = UIColor(red: 212.0/255.0, green: 87.0/255.0, blue: 80.0/255.0, alpha: 1.0) // alpha 1.0 = fully opaque (vs. transparent)
+    static var backgroundGray = UIColor(red: 45.0/255.0, green: 45.0/255.0, blue: 45.0/255.0, alpha: 1.0)
+    
+    static func typerighterFont(with textStyle: UIFont.TextStyle, pointSize: CGFloat) -> UIFont {
+        let font = UIFont(name: "RM Typerighter medium", size: pointSize)! // force unwrap out of simplicity, should probably return an optional UIFont in practice
+        return UIFontMetrics(forTextStyle: textStyle).scaledFont(for: font) // has to do with accessibility for larger text in settings in iPhone (dynamic type)
+        // very important part of building an app as 100's of thousands of people use dynamic font
+    }
+    
+    static func setDarkAppearance() {
+        UINavigationBar.appearance().barTintColor = backgroundGray
+        UISegmentedControl.appearance().tintColor = lambdaRed
+        UIBarButtonItem.appearance().tintColor = lambdaRed
+        
+        let textAttributes = [NSAttributedString.Key.foregroundColor: UIColor.white]
+        
+        UINavigationBar.appearance().titleTextAttributes = textAttributes
+        UINavigationBar.appearance().largeTitleTextAttributes = textAttributes
+        
+        UITextField.appearance().tintColor = lambdaRed
+        UITextView.appearance().tintColor = lambdaRed
+        
+    }
+    
+    static func style(button: UIButton) {
+        button.titleLabel?.font = typerighterFont(with: .callout, pointSize: 30.0)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = lambdaRed
+        button.layer.cornerRadius = 8
+    }
+    
+}
--- Lambda Messages Theming Starter/Lambda Messages Theming/View Controllers/SignupViewController.swift
+++ Lambda Messages Theming Starter/Lambda Messages Theming/View Controllers/SignupViewController.swift
@@ -12,6 +12,7 @@ class SignupViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setUpAppearance()
 
         guard AuthenticationHelper.currentUser != nil else { return }
         
@@ -24,6 +25,13 @@ class SignupViewController: UIViewController {
         AuthenticationHelper.setCurrentUser(to: username)
         
         performSegue(withIdentifier: "ViewMessageList", sender: nil)
+    }
+    
+    private func setUpAppearance() {
+        view.backgroundColor = AppearanceHelper.backgroundGray
+        usernameTextField.font = AppearanceHelper.typerighterFont(with: .callout, pointSize: 28.0)
+        AppearanceHelper.style(button: signupButton)
+        signupButton.setTitle("Sign Up", for: .normal)
     }
     
     @IBOutlet weak var signupButton: UIButton!
